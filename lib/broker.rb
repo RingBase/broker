8cc408ad6a86b8b5275e7cc4ef0157f5149690b0
Andrew Berls <andrew.berls@gmail.com>
2014-02-02 20:32:25 -0800

    Setup 2-way AMQP and Invoca pub/sub simulators
    The Broker will need to listen for AMQP messages as well as publishing
    them. This is implemented for now using two separate queues, although
    I'll need to read more about AMQP to see if there's a more semantic way.
    I'm not wild about the queue names either.
    
    Two scripts are added to `invoca/` to simulate sending/receiving AMQP
    messages from the Invoca API end.

--- lib/broker.rb
+++ lib/broker.rb
@@ -13,23 +13,16 @@ class Broker
 
   Channels = {}
 
-  # TODO: probably will want to load config from YAML or similar
-  QUEUE_NAME = "hello_world"
-
   class << self
-    attr_accessor :bunny, :ch, :ex, :q
-
     def config
       @config ||= YAML.load_file("config.yml")
     end
 
     def publish(msg)
-      raise "Not connected!" unless self.bunny.connected?
-      self.ex.publish(msg, routing_key: config["queue_name"])
+      raise "Not connected!" unless @bunny.connected?
+      @ex.publish(msg, routing_key: "broker_to_invoca")
     end
 
-    # TODO: Goliath nomally starts the reactor itself.
-    # Is there any reason we can't wrap it in our own run loop?
     def run!
       EM.run do
         connect_amqp!
@@ -37,18 +30,22 @@ class Broker
       end
     end
 
-    # TODO: handle connection/auth errors
+    # Connect Bunny to the AMQP broker and set up the inbound listener queue
     def connect_amqp!
       username = config['username']
       password = config['password']
       host     = config['host']
       port     = config['port']
-      self.bunny = Bunny.new("amqp://#{username}:#{password}@#{host}:#{port}")
-      self.bunny.start
+      @bunny = Bunny.new("amqp://#{username}:#{password}@#{host}:#{port}")
+      @bunny.start
 
-      self.ch = bunny.create_channel
-      self.ex = ch.default_exchange
-      self.q  = ch.queue(config["queue_name"], auto_delete: true)
+      @ch = @bunny.create_channel
+      @ex = @ch.default_exchange
+      @q  = @ch.queue("invoca_to_broker", auto_delete: true)
+
+      @q.subscribe do |delivery_info, metadata, payload|
+        puts "BROKER GOT: #{payload}"
+      end
     end
 
     def run_app!

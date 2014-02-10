4d16a874324359869b0a8ab6d3e8b139349a38e1
Andrew Berls <andrew.berls@gmail.com>
2014-02-09 19:17:53 -0800

    Attempt to bridge Broker-Invoca / Broker-Client sides
    The Broker maintains a connection to Invoca via AMQP, and connections to
    browser clients via websockets. However, for anything useful to be
    accomplished, these two sides must be able to talk to each other, a task
    which is surprisingly difficult.
    
    Goliath performs some interesting tricks by default. For example, the
    default behavior is to start the server in an `at_exit` handler, so all
    you have to do is define your server class and it will automatically run
    and "just work". As a consequence of this, the actual server object is
    constructed internally within Goliath and it is nigh impossible to get a
    programmatic handle to it from anywhere outside. The ramificiations of
    this are as follows:
    
    Goliath communicates with browser clients through *instance* methods defined
    on the server class, e.g. `on_open`, etc. The Broker-Invoca side of things
    communicates with the server using *class* methods, e.g.
    `Broker::SocketServer.handle_call_start`. However, since the server
    instance is only referenced internally within Goliath, there is no way
    to bridge these two things!
    
    I can think of virtually no solution to this problem without modifying
    the internals of Goliath. So we'll do the next best thing - rip out the
    necessary lines from the Goliath source and paste them into our own so
    we can save a reference to the server instance!
    
    Just kidding, that's not the next best thing, this code is garbage. It'll
    have to do for now until I can come up with a better solution.

--- lib/broker.rb
+++ lib/broker.rb
@@ -12,6 +12,8 @@ Goliath.run_app_on_exit = false
 module Broker
   extend self
 
+  attr_accessor :server, :logger
+
   def config
     @config ||= YAML.load_file("config.yml")
   end
@@ -29,10 +31,10 @@ module Broker
 
   # Connect Bunny to the AMQP broker and set up the inbound listener queue
   def connect_amqp!
-    username = config['username']
-    password = config['password']
-    host     = config['host']
-    port     = config['port']
+    username = config['rabbitmq']['username']
+    password = config['rabbitmq']['password']
+    host     = config['rabbitmq']['host']
+    port     = config['rabbitmq']['port']
     @bunny = Bunny.new("amqp://#{username}:#{password}@#{host}:#{port}")
     @bunny.start
 
@@ -46,12 +48,38 @@ module Broker
     end
   end
 
+  def setup_logger
+    logger     = Log4r::Logger.new('goliath')
+    log_format = Log4r::PatternFormatter.new(:pattern => "[#{Process.pid}:%l] %d :: %m")
+    logger.add(Log4r::StdoutOutputter.new('console', formatter: log_format))
+    logger.level = Log4r::DEBUG
+    self.logger = logger
+    logger
+  end
+
+  def setup_server
+    api = Broker::SocketServer.new
+    app = Goliath::Rack::Builder.build(Broker::SocketServer, api)
+
+    address = config['server']['address']
+    port    = config['server']['port']
+    server  = Goliath::Server.new(address, port)
+
+    server.logger  = setup_logger
+    server.app     = app
+    server.api     = api
+    server.plugins = []
+    server.options = {}
+    server.api.setup if server.api.respond_to?(:setup)
+
+    self.server = server
+    server
+  end
+
   def run_app!
-    # TODO: massive hack to enable stdout logging, since it doesn't appear we can
-    # change settings programatically. Eventually we'll want a more robust
-    # logging solution anyways
-    ARGV[1] = "-sv"
-    Goliath::Application.run!
+    setup_server
+    logger.info("Starting customized server on #{address}:#{port}. Watch out for stones.")
+    self.server.start
   end
 
 end
